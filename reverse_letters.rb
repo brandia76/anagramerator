d180be14dbfba76c1de5dbd1ee4bc415845093db
Brandi <brandi.bermann@gmail.com>
2015-11-20 23:14:05 -0500

    Delete reverse_letters.rb

--- reverse_letters.rb
+++
@@ -1,9 +0,0 @@
-def reverse_letters(arr)
-  size = arr.length
-  new_arr = Array.new(size)
-  
-  arr.each_with_index do |letter, index|
-    new_arr[size-index-1] = letter
-  end
-  return new_arr
-end
