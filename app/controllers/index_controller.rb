faf6a790b8206a82cc47acf5d0cb954d8d7cd5b7
Brandi Bermann <brandi.bermann@gmail.com>
2015-11-22 23:05:03 -0500

    add index page for words in database

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -12,7 +12,8 @@ end
 
 get '/anagrams/:word' do  
   @word = params[:word]
-  @anagrams = Word.find_anagrams(@word)
+  alphabetized_string = @word.chars.sort.join
+  @anagrams = Word.where("letters=?", alphabetized_string) #Word.find_anagrams(@word)
   erb :show
 end
 
